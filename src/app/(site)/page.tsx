bac249b46d4e78ea38cea22972d83bcc33e69f55
ogmjbrand <Kharmony987@gmail.com>
2026-08-12 20:10:13 +0100

    feat: switch homepage to new ByChi editorial commerce design

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,10 @@
-import { LuxuryHome } from "@/components/flagship/LuxuryHome";
+import { HouseHomepage } from "@/components/flagship/HouseHomepage";
 import styles from "./bychi-colors.module.css";
 
 export default function HomePage() {
   return (
     <div className={styles.system}>
-      <LuxuryHome />
+      <HouseHomepage />
     </div>
   );
 }
